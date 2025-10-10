c6e2fac716ddece7adb86a7c75c25550067227f0
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-10-10 15:52:36 +0200

    TranslationHelper - Add "Move first names to front" setting

--- tools/AC_TranslationHelper/Extensions.cs
+++ tools/AC_TranslationHelper/Extensions.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections;
 
-namespace ReleaseTool
+namespace AC_TranslationHelper
 {
     internal static class Extensions
     {
